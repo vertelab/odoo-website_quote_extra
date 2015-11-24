7d2c0c33c29fdef575d8dd6be8047a0aaee4ac02
christelle wehbe <wehbechristelle@gmail.com>
2015-11-24 16:06:16 +0100

    new module: website_quote_table_desc

---
+++ website_quote_table_desc/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#
+#
+##############################################################################
+
+
+{
+    'name': 'Website Quotation Table Description',
+    'version': '1.1',
+    'category': 'other',
+    'summary': 'Adds description and image to quotation table',
+    'author': 'Vertel AB',
+    'website': 'http://www.vertel.se',
+    'depends': ['website_quote', 'product'],
+    'data': ['website_quotation_table.xml'],
+    'installable': True,
+    'application': False,
+    #'auto_install': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
